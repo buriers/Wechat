21da048ae6a96bd0864f073f0c9b8c54b4bf6043
kele.com <315136803@qq.com>
2019-05-16 11:22:33 +0800

    文件路由算法

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
-echo "hello world";
+echo "hell world";
 echo "这里创建文件 vi 文件名, git add 文件名跟踪";
-
+namespace model;
+include "model.php";
+spl_autoload_reqister(function ($className)
+{include str_replace("\\","/",$className.".php")});
+use function model\
 ?>
 
